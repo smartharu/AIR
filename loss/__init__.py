e8cbf223ce1eec171ce396d1b140e98dcd91e137
smartharu <69776858+smartharu@users.noreply.github.com>
2024-11-04 23:14:00 +0800

    add gradient loss

---
+++ loss/__init__.py
@@ -0,0 +1,7 @@
+from gradient_loss import GradientLoss
+from clamp_loss import ClampLoss
+from lbp_loss import YL1LBP,YLBP
+
+__all__ = ["GradientLoss",
+           "ClampLoss",
+           "YL1LBP","YLBP"]
